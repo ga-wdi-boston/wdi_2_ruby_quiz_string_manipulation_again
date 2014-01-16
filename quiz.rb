e33d903455ce72d8eed3bb81b5ae8a5f0e5ccbcf
Jack Henley <jack.d.henley@gmail.com>
2014-01-16 09:45:33 -0500

    all set but small error on regex

--- quiz.rb
+++ quiz.rb
@@ -11,7 +11,14 @@
 # You can sleep in if it is not a weekday or if you are on vacation.
 # sleep_in? => false
 # sleep_in?(vacation: true) => true
-def sleep_in?
+def sleep_in?(options = {})
+	if Time.now.wday == 6 or Time.now.wday == 7
+		return true
+	elsif options[:vacation] == true
+		return true
+	else
+		return false
+	end
 end
 
 # Question 2: a method called del_del
@@ -20,12 +27,22 @@ end
 # del_del("abdelcd") => "abcd"
 # del_del("xyz") => "xyz"
 
+def del_del(string)
+	string.delete('/["del"]/')
+end
+
 # Question 3: a method called missing_char
 #############
 # Remove the character that corresponds to the index from the string.
 # If you don't enter a string
 # missing_char("kitten", 1) => "ktten"
 # missing_char(347, 1) => RuntimeError: Please enter a string!
+
+def missing_char(string, index)
+	array = string.split(//)
+	array.delete_at(index)
+	array.join
+end
 
 # Question 4: a method called near_hundred?
 #############
@@ -36,8 +53,26 @@ end
 # near_hundred?(52) => false
 # near_hundred?('two') => RuntimeError: Please enter a number!
 
+def near_hundred(num)
+	range = (90..110)
+	if range.include?(num)
+		true
+	else
+		false
+	end
+end
+
 # Question 5: a method called back_around
 #############
 # Given a string, move the last character to the beginning.
 # "cat".back_around => "tca"
 # "hello".back_around => "ohell"
+
+def back_around(string)
+	array = string.split(//)
+	last = array.pop
+	array.unshift(last)
+	array.join
+end
+
+
