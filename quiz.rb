6bccaf213bba106cc53e1175084ac15954691796
Laure Fischer <laurefischer27@gmail.com>
2014-01-16 09:43:00 -0500

    initial commit

--- quiz.rb
+++ quiz.rb
@@ -11,7 +11,14 @@
 # You can sleep in if it is not a weekday or if you are on vacation.
 # sleep_in? => false
 # sleep_in?(vacation: true) => true
-def sleep_in?
+def sleep_in?(options = {})
+  if Time.now.wday == 6  || Time.now.wday == 0
+    true
+  elsif options[:vacation] == true
+    true
+  else
+    false
+  end
 end
 
 # Question 2: a method called del_del
@@ -19,6 +26,9 @@ end
 # Remove "del" from a string.
 # del_del("abdelcd") => "abcd"
 # del_del("xyz") => "xyz"
+def del_del(string)
+  string.delete('del')
+end
 
 # Question 3: a method called missing_char
 #############
@@ -26,7 +36,9 @@ end
 # If you don't enter a string
 # missing_char("kitten", 1) => "ktten"
 # missing_char(347, 1) => RuntimeError: Please enter a string!
-
+def missing_char(string, index)
+  string.delete(string[index])
+end
 # Question 4: a method called near_hundred?
 #############
 # Write a method called near_hundred?
@@ -35,9 +47,21 @@ end
 # near_hundred? => true
 # near_hundred?(52) => false
 # near_hundred?('two') => RuntimeError: Please enter a number!
-
+def near_hundred(number)
+  if (100 - number).abs < 10
+    true
+  else
+    false
+  end
+end
 # Question 5: a method called back_around
 #############
 # Given a string, move the last character to the beginning.
 # "cat".back_around => "tca"
 # "hello".back_around => "ohell"
+
+def back_around(string)
+  last_character = string.last
+  new_string = string.delete(last_character)
+  last_character + new_string
+end
